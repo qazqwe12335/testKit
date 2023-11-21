aa7a074a2d133d86db5de75c11b0f35d45dea50c
qazqwe12335 <34392366+qazqwe12335@users.noreply.github.com>
2023-11-21 16:12:13 +0800

    Update Package.swift

--- InterinfoKit/Package.swift
+++ InterinfoKit/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.5
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -18,7 +18,7 @@ let package = Package(
         .target(
             name: "InterinfoKit",
             dependencies: [],
-            path: "Sources/InterinfoKit"),
+            path: "Sources"),
         .testTarget(
             name: "InterinfoKitTests",
             dependencies: ["InterinfoKit"]),
